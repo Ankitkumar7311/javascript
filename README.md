c43cedf29626463ea9b9cda51d2149f5e7e45076
Ankit Kumar <143241335+Ankitkumar7311@users.noreply.github.com>
2024-05-03 03:35:36 +0000

    update with day 2

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # javascript
 javascript-code-with-chai
+  
+
+
+   DAY = 1
+// lecture 1 = introduction
+lecture 2 = installation of node js
+lecture 3 = vs code codespace
+lecture 4 = variables let ,const , var ki kahani
+lecture 5 = data types 
+lecture 6 =  conversion operation
+lecture 7  = why string conversion in js is trickky
+
+
+
+  DAY = 2
+lecture 8 = comparison of datatypes in javascript
---
+++ javascript/day1.js
@@ -0,0 +1,54 @@
+// let a=55;
+// console.log(a);
+//  a =65 ;
+// console.log(typeof a);
+
+
+// let Name="ankit"
+// console.log(typeof Name)
+
+
+// var Name="ankit"
+//  console.log(typeof Name)
+
+
+// const password= 123456
+// console.log(password)
+// console.log(typeof password)
+
+
+// DAY = 2
+
+
+// var name= null
+// console.log(typeof name)
+
+
+
+// console.log("2"==2)   // false
+// console.log("2"===2)  // true
+
+
+// console.log("2"+2)
+// console.log("2"+"2")
+
+
+// console.log(undefined == true)
+// console.log(undefined == false)
+// console.log(undefined == null) // true
+// console.log(undefined == 0)
+
+
+// console.log(2>=3)   false
+//console.log(3==3)   false
+// console.log(2===3)  false
+// console.log(2<=3)   true
+// console.log("2"==3)    false
+ 
+
+
+
+
+
+
+
